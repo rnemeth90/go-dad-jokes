91e4056bb17688cebb139e47afa12fa0322b4638
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-07-21 13:15:38 -0400

    working poc

--- main.go
+++ main.go
@@ -1,67 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-    router.GET("/albums/:id", getAlbumByID)
-    router.POST("/albums", postAlbums)
-
-    router.Run("localhost:8080")
+	router := gin.Default()
+	router.GET("/", getRandomJoke)
+	router.Run("localhost:8080")
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
-}
+func getRandomJoke(c *gin.Context) {
+	rand.Seed(time.Now().Unix())
+	log.SetPrefix("getRandomJoke(): ")
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-    var newAlbum album
+	jsonFile, err := ioutil.ReadFile("./jokes.json")
+	if err != nil {
+		log.Fatal("Error opening the file: ", err)
+	}
 
-    // Call BindJSON to bind the received JSON to
-    // newAlbum.
-    if err := c.BindJSON(&newAlbum); err != nil {
-        return
-    }
-
-    // Add the new album to the slice.
-    albums = append(albums, newAlbum)
-    c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-    id := c.Param("id")
-
-    // Loop through the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, a := range albums {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
-            return
-        }
-    }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	jokes := make([]string, 0)
+	err = json.Unmarshal(jsonFile, &jokes)
+	if err != nil {
+		log.Fatal("Error decoding json: ", err)
+	}
+	n := rand.Int() % len(jokes)
+	c.IndentedJSON(http.StatusOK, jokes[n])
 }
